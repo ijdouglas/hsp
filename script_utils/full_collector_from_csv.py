c0acc62978baae147230c54b0922f390fb07bd3f
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2020-07-13 11:47:32 -0400

    finalized compiler for different conditions

--- script_utils/full_collector_from_csv.py
+++ script_utils/full_collector_from_csv.py
@@ -5,14 +5,14 @@ from spellchecker import SpellChecker
 from nltk.stem import SnowballStemmer
 from nltk.corpus import wordnet as wn
 from nltk.stem.wordnet import WordNetLemmatizer
-
-#dictionary part:
-
-
+from datetime import date
 
 spell = SpellChecker()
+
 stemmer = SnowballStemmer("english")
+
 lm = WordNetLemmatizer()
+
 
 dir = "c:/Users/Ellis/Desktop/Lab/hsp_results/cond9_syntax1/"
 input_data = [["subj#","trial#","target","original_guess","corrected_guess","stem","match"]]
@@ -51,6 +51,13 @@ def extract_baseline_video(word):
     word = word.replace("sample/","")
     word = word.replace("hammer/h/","")
     word = word.replace("data/final_selection/","")
+    word = word[word.find("/")+1:]
+    return word
+def extract_cond45_video(word):
+    word = word.replace("sample/","")
+    word = word.replace("hammer/h/","")
+    word = word.replace("data/final_selection/","")
+    word = word.replace("../hsp_verbs_mturk3/","")
     word = word[word.find("/")+1:]
     return word
 def extract_baseline_word(word):
@@ -101,7 +108,7 @@ def sona_generate(dir):
     condition = condition.replace("/","")
     for root, dirs, files in os.walk(dir):
         for file in files:
-            if "syntax" in root:
+            if "syntax" in root and "experiment" in file:
                 temp = list(csv.reader(open(os.path.join(root,file), encoding="utf8")))
                 #print(os.path.join(root, file))
                 for row in temp:
@@ -155,7 +162,7 @@ def turk_generate(dir):
     condition = condition.replace("/","")
     for root, dirs, files in os.walk(dir):
         for file in files:
-            if "syntax" in root and "ignore" not in file:
+            if "syntax" in root and "ignore" not in file and "experiment" in file:
                 temp = list(csv.reader(open(os.path.join(root,file), encoding="utf8")))
                 #print(os.path.join(root, file))
                 for row in temp:
@@ -212,22 +219,61 @@ def turk_generate(dir):
                         input_data.append([ip,condition,trial,block_set,block_num,target,filename["words"][0],guess,int(filename["words"][0]==guess),corrected,candidates,spell.word_probability(corrected),int(guess==corrected),stemm,lemma,int(guess==stemm),int(target==stemm),int(verbs>0)])
     return input_data
 def cond45_generate(dir):
-    input_data = [["subj#","trial#","target","guess"]]
+    print("test")
+    print(dir)
+    input_data = [["subj#","condition","trial#","block_set","block_num","target","original_guess","corrected_guess","og_cg_match","spell-check","candidates","wordprob","cg_sc_match","stem","lemm","cg_st_match","target_stem_match","verb_synset"]] #block #, likely verb?
+    condition = dir.replace("c:/Users/Ellis/Desktop/Lab/hsp_results/","")
+    condition = condition.replace("/","")
     for root, dirs, files in os.walk(dir):
         for file in files:
-            if "experiment_data.csv" in file:
-                filepath = os.path.join(root, file)
-                #print(filepath)
-                temp = list(csv.reader(open(filepath, encoding="utf8")))
+            if "block" in root and "experiment" in file:
+                temp = list(csv.reader(open(os.path.join(root,file), encoding="utf8")))
+                #print(os.path.join(root, file))
                 for row in temp:
-                    if row[0] != "rt" and len(row[9])!=0:
+                    if row[0] != "rt" and len(row[9])!= 0:
+                        ip = row[7]
+                        if len(row[7]) == 0:
+                            ip = "undefined"
                         filename = json.loads(row[9])
                         video = filename["video"]
-                        video = video.replace("../hsp_verbs_mturk3/data/final_selection/","")
-                        video = video.replace("hammer/h/","")
-                        video = video.replace(".mp4","")
-                        video = video[video.find("/")+1:]
-                        input_data.append([row[7],video,extract_cond45_word(video),filename["words"][0]])
+                        print(video)
+                        trial = extract_cond45_video(video)
+                        target = extract_cond45_word(video)
+                        guess = basic_corrections(filename["words"][0].strip()) #????????
+                        if " " in filename["words"][0].strip() and filename["words"][0].strip() not in spaces:
+                            spaces.append([filename["words"][0],guess])
+                            spaces_other.append(row)
+                            '''
+                            print("they enter: ",filename["words"][0])
+                            print("we corrected to: ",guess)
+                            x = input("Is that correct (y/n)? ")
+                            if x.lower() == "y" or x.lower() == "yes":
+                                print("Ok, we will save: ", guess)
+                            elif x.lower() == "n" or x.lower() == "no":
+                                y = input("Please enter the correction: ")
+                                z = input("is ",y," correct? (y/n)")
+                                if z.lower() == "y" or z.lower() == "yes":
+                                    print("Ok, we will save: ", y)
+                                else:
+                                    print("We will save: ",guess)
+                            print()
+                            '''
+                        is_sample = "_s_" in trial
+                        if is_sample:
+                            block_num = 0
+                            block_set = 0
+                        elif block_num == 6:
+                            block_num = 1
+                        else:
+                            block_num = block_num +1
+                        if block_num == 1:
+                            block_set += 1
+                        corrected = spell.correction(guess) # no entry goes to a, a
+                        stemm = stemmer.stem(corrected) #fell vs fall, fell a tree  --- alternate to altern?
+                        candidates = spell.candidates(guess)
+                        verbs = len(wn.synsets(stemm, pos=wn.VERB))
+                        lemma = lm.lemmatize(corrected, wn.VERB)
+                        input_data.append([ip,condition,trial,block_set,block_num,target,filename["words"][0],guess,int(filename["words"][0]==guess),corrected,candidates,spell.word_probability(corrected),int(guess==corrected),stemm,lemma,int(guess==stemm),int(target==stemm),int(verbs>0)])
     return input_data
 def baseline_generate(dir):
     input_data = [["subj#","condition","trial#","block_set","block_num","target","original_guess","corrected_guess","og_cg_match","spell-check","candidates","wordprob","cg_sc_match","stem","lemma","cg_st_match","target_stem_match","verb_synset"]]
@@ -237,7 +283,7 @@ def baseline_generate(dir):
         for file in files:
             if "experiment_data.csv" in file:
                 filepath = os.path.join(root, file)
-                #print(filepath)
+                print(filepath)
                 temp = list(csv.reader(open(filepath, encoding="utf8")))
                 for row in temp:
                     if row[0] != "rt" and len(row[9])!=0:
@@ -279,74 +325,93 @@ def write_data(arrayed,name):
         writer.writerows(arrayed)
 
 def general_walk(root_dir):
+    today = date.today()
+    date = today.strftime("&d_%m_%Y")
     dir = root_dir
     for root, dirs, files in os.walk(dir):
         #condition = dir.replace("c:/Users/Ellis/Desktop/Lab/hsp_results/","")
             #condition = condition.replace("/","")
         if "cond1" in root and "syntax" not in root:
             print("condition_1")
-            data = baseline_generate(root)
-            #for row in data:
-            #    print(row)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data = baseline_generate(root)
+                filename = "cond1_"+date+".csv"
+                write_data(data,filename)
+            #    for row in data:
+            #        print(row)
         elif "cond2" in root:
             print("condition_2")
-            data2 = baseline_generate(root)
-            #for row in data2:
-            #    print(row)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data2 = baseline_generate(root)
+                filename = "cond2_"+date+".csv"
+                write_data(data2,filename)
+            #    for row in data2:
+            #        print(row)
         elif "cond3" in root:
             print("condition_3")
-            data3 = baseline_generate(root)
-            #for row in data3:
-            #    print(row)
-        elif "cond4" in root:
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data3 = baseline_generate(root)
+                filename = "cond3_"+date+".csv"
+                write_data(data3,filename)
+            #    for row in data3:
+            #        print(row)
+        elif "cond4" in root: 
             print("condition_4")
-            data4 = sona_generate(root)
-            for row in data4:
-                print(row)
             print(root)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data4 = cond45_generate(root)
+                filename = "cond4_"+date+".csv"
+                write_data(data4,filename)
+                #for row in data4:
+                #    print(row)
         elif "cond5" in root:
             print("condition_5")
-            print(root)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data5 = cond45_generate(root)
+                filename = "cond5_"+date+".csv"
+                write_data(data5,filename)
         elif "cond7" in root:
             print("condition_7")
-            data7 = baseline_generate(root)
-            for row in data7:
-                print(row)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data7 = baseline_generate(root)
+                filename = "cond7_"+date+".csv"
+                write_data(data7,filename)
+                #for row in data7:
+                #    print(row)
         elif "cond8" in root:
             print("condition_8")
-            data8 = baseline_generate(root)
-            for row in data8:
-                print(row)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                data8 = baseline_generate(root)
+                filename = "cond8_"+date+".csv"
+                write_data(data8,filename)
+                #for row in data8:
+                #    print(row)
         elif "cond9_syntax1" in root and "turk" not in root:
             print("sona")
-            #sona_data = sona_generate(root)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                sona_data = sona_generate(root)
+                filename = "cond9_sona_"+date+".csv"
+                write_data(sona_data,filename)
+                #for row in sona_data:
+                #    print(row)
         elif "cond9_syntax1" in root and "turk" in root:
             print("turk")
-            #turk_data = turk_generate(root)
+            x = input("run?")
+            if x == "y" or x == "yes" or x == "1":
+                turk_data = turk_generate(root)
+                filename = "cond9_turk_"+date+".csv"
+                write_data(turk_data,filename)
+                #for row in turk_data:
+                #    print(row)
+            #write_data(turk_data, "new_turk_Data.csv")
 
 
 general_walk("c:/Users/Ellis/Desktop/Lab/hsp_results/")
-
-#sona portion
-'''
-sona_dir = "c:/Users/Ellis/Desktop/Lab/hsp_results/cond9_syntax1/"
-sona_data = sona_generate(sona_dir)
-#write_data(sona_data,"sona_syntax_data.csv")
-#print(sona_data)
-
-
-print()
-
-#Turk portion
-
-turk_dir = "c:/Users/Ellis/Desktop/Lab/hsp_results/cond9_syntax1_turk/"
-turk_data = turk_generate(turk_dir)
-#write_data(turk_data,"turk_syntax_data.csv")
-#print(turk_data)
-for row in spaces:
-    print(row)
-for row in turk_data:
-    print(row)
-'''
-
-#flag space
