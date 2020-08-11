fc84a59545eacabf6137c208365249e1f8e8e3e3
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2020-08-11 16:26:20 -0400

    Updated to do random sampling

--- exps/hsp_word_pairs/hsp.js
+++ exps/hsp_word_pairs/hsp.js
@@ -48,7 +48,16 @@ var instructions3 = {
 
     <p><b><i>Press any key to begin.</i></b></p>`
 };
-``
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array;
+}
 
 var end_training = {
     type: 'html-keyboard-response',
