4eb409ce4b08e91b31cbf2249b79ca5bfa0c0a38
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2021-01-18 08:59:01 -0500

    Added scripts
    Audio extract.py -> as the name suggests, extracts audio from videos using ffmpeg
    
    json2matlab.py -> converts bbox.json files (for exp15) to matlab files for parent and child
    
    verb_sim_cleaner.py -> basic script to clean data from mturk verb sim exp

---
+++ script_utils/json2matlab.py
@@ -0,0 +1,128 @@
+import json
+import os
+import scipy.io as sio
+import numpy as np
+
+subj_dict = {"20241":"1501","20427":"1502","20142":"1503","21933":"1504","20582":"1505","20695":"1506","21633":"1507","20473":"1508","21278":"1509","21992":"1510","20674":"1511","23512":"1512","21840":"1513","24257":"1514","21501":"1515","21960":"1516","23639":"1517","22944":"1518","21699":"1519","21769":"1520","21625":"1521","19544":"1522","19946":"1523","22049":"1524","21908":"1525","23513":"1526","21827":"1527","22666":"1528","23363":"1529","21977":"1530","23376":"1531","21131":"1532","24234":"1533","23523":"1534","23674":"1535","21381":"1536","22655":"1537","22791":"1538","23778":"1539","23846":"1540","23301":"1541","23757":"1542","24642":"1543","23775":"1544","24031":"1545","24158":"1546","24335":"1547","24270":"1548","24836":"1549","24368":"1550","24414":"1551","24237":"1552","24842":"1553","24416":"1554"}
+
+#json_filepath = "/data/aamatuni/code/postprocess_boxes/output/bbox_processed.json"
+
+child_frame_vals = {}
+parent_frame_vals = {}
+child_frame2bbox = {}
+parent_frame2bbox = {}
+
+
+search_dir = "/data/aamatuni/code/postprocess_boxes/output/"
+'''
+for root, dirs, files in os.walk(search_dir):
+	for file in files:
+		if ".json" in file:
+			filepath = os.path.join(root, file)
+			json_file = open(filepath)
+			json_data = json.load(json_file)
+'''
+#dict_keys(['image_id', 'category_id', 'bbox', 'score', 'fnum', 'fname'])
+#check metadata on the ones on the server
+#recheck one of the exp 12 mat files and replicate the data structure
+
+json_data = json.load(open("/data/aamatuni/code/postprocess_boxes/output/exp15_remaining/bbox.json"))
+
+for row in json_data:
+	#print(search_dir, row["fname"])
+	directory = search_dir+row["fname"]
+	name = row["fname"]
+	name = name[name.find("_")+1:]
+	name = name[:name.find("_")]
+	if name[-5:] in subj_dict.keys():
+		subj_id = subj_dict[name[-5:]]
+		#print(directory[directory.find("_"):])
+		fnum = row["fnum"]
+		#print(fnum)
+		#print(row["bbox"])
+		converted_bbox = []
+		x = int(row["bbox"][0])
+		y = int(row["bbox"][1])
+		w = int(row["bbox"][2])
+		h = int(row["bbox"][3])
+		converted_bbox.append(x/640) #try to grab from file
+		converted_bbox.append(y/480)
+		converted_bbox.append(w/640)
+		converted_bbox.append(h/480)
+		#print(converted_bbox)	
+		cat = row["category_id"]
+		index = int(cat) -1
+		#child
+		if subj_id in child_frame2bbox and "cam08" not in directory:
+			if fnum in child_frame2bbox[subj_id]:
+				child_frame2bbox[subj_id][fnum][index] = converted_bbox
+			elif fnum not in child_frame2bbox[subj_id]:
+				child_frame2bbox[subj_id][fnum] = np.zeros((12,4))
+				child_frame2bbox[subj_id][fnum][index] = converted_bbox
+		elif subj_id not in child_frame2bbox and "cam08" not in directory:
+			child_frame2bbox[subj_id] = {}
+			if fnum in child_frame2bbox[subj_id]:
+				child_frame2bbox[subj_id][fnum][index] = converted_bbox
+			elif fnum not in child_frame2bbox[subj_id]:
+				child_frame2bbox[subj_id][fnum] = np.zeros((12,4))
+				child_frame2bbox[subj_id][fnum][index] = converted_bbox
+		#parent
+		if subj_id in parent_frame2bbox and "cam08" in directory:
+			if fnum in parent_frame2bbox[subj_id]:
+				parent_frame2bbox[subj_id][fnum][index] = converted_bbox
+			elif fnum not in parent_frame2bbox[subj_id]:
+				parent_frame2bbox[subj_id][fnum] = np.zeros((12,4))
+				parent_frame2bbox[subj_id][fnum][index] = converted_bbox
+		elif subj_id not in parent_frame2bbox and "cam08" in directory:
+			parent_frame2bbox[subj_id] = {}
+			if fnum in parent_frame2bbox[subj_id]:
+				parent_frame2bbox[subj_id][fnum][index] = converted_bbox
+			elif fnum not in parent_frame2bbox[subj_id]:
+				parent_frame2bbox[subj_id][fnum] = np.zeros((12,4))
+				parent_frame2bbox[subj_id][fnum][index] = converted_bbox
+		#child
+		if subj_id in child_frame_vals and "cam08" not in directory:
+			if fnum not in child_frame_vals[subj_id]:
+				child_frame_vals[subj_id][fnum] = directory
+		elif subj_id not in child_frame_vals and "cam08" not in directory:
+			child_frame_vals[subj_id] = {}
+			if fnum not in child_frame_vals[subj_id]:
+				child_frame_vals[subj_id][fnum] = directory
+		#parent
+		if subj_id in parent_frame_vals and "cam08" in directory:
+			if fnum not in parent_frame_vals[subj_id]:
+				parent_frame_vals[subj_id][fnum] = directory
+		elif subj_id not in parent_frame_vals and "cam08" in directory:
+			parent_frame_vals[subj_id] = {}
+			if fnum not in parent_frame_vals[subj_id]:
+				parent_frame_vals[subj_id][fnum] = directory
+
+
+
+for subject in child_frame_vals.keys():
+	final_mat_dict = []
+	subj = subject
+	save_string = subj+"_child_boxes.mat"
+	frames = child_frame_vals[subj].keys()
+	frames = sorted(frames)
+	for frame in frames:
+		direc = child_frame_vals[subj][frame]
+		bboxes = child_frame2bbox[subj][frame]
+		#[directory, -, fnum, [bboxes]]		
+		final_mat_dict.append([[direc], [0], [frame], bboxes])
+	dictionary = {'__header__': b'MATLAB 5.0 MAT-file, Platform: GLNXA64, Created on: Tue Mar 03 14:02:59 2020', '__version__': '1.0', '__globals__': [], 'box_data': final_mat_dict}
+	sio.savemat(save_string, dictionary)
+
+for subject in parent_frame_vals.keys():
+	final_mat_dict = []
+	subj = subject
+	save_string = subj+"_parent_boxes.mat"
+	frames = parent_frame_vals[subj].keys()
+	frames = sorted(frames)
+	for frame in frames:
+		direc = parent_frame_vals[subj][frame]
+		bboxes = parent_frame2bbox[subj][frame]
+		final_mat_dict.append([[direc], [0], [frame], bboxes])
+	dictionary = {'__header__': b'MATLAB 5.0 MAT-file, Platform: GLNXA64, Created on: Tue Mar 03 14:02:59 2020', '__version__': '1.0', '__globals__': [], 'box_data': final_mat_dict}
+	sio.savemat(save_string, dictionary)
+
---
+++ script_utils/verb_sim_cleaner.py
@@ -0,0 +1,69 @@
+import json
+import pandas as pd
+import os
+import csv
+import numpy as np
+
+def write_data(arrayed,name):
+    with open(name,mode="w",encoding="utf-8",newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(arrayed)
+
+
+dictionary_csv = list(csv.reader(open("hsp_voc.csv")))
+word_dict = {}
+for line in dictionary_csv:
+    arr = line
+    val = int(arr[1])
+    word = arr[0]
+    word_dict[word] = val
+
+avg_dict = {}
+
+walk_location = "/mnt/c/Users/wkw/Documents/Lab/verb_similarity_data/"
+target_dir = "/mnt/c/Users/wkw/Desktop/"
+
+
+full_norm = [["id","wordA","wordB","sim_distance"]]
+full_norm_id = [["id","wordA_id","wordB_id","sim_distance"]]
+avg_norm = [["wordA","wordB","avg_distance"]]
+avg_norm_id = [["wordA_id","wordB_id","avg_distance"]]
+
+dir = walk_location
+idd = 1
+for root, dirs, files in os.walk(dir):
+    if "results" in root:
+        for file in files:
+            if "experiment_data.csv" in file and "ignore" not in file:
+                temp = pd.read_csv(os.path.join(root,file))
+                #print(root,file)
+                if "part1" in root:
+                    upperbound = 167
+                elif "part2" or "part3" in root:
+                    upperbound = 168
+                for row in temp["responses"].iloc[3:upperbound]:
+                    dictt = json.loads(row)
+                    for key, value in dictt.items():
+                        wordA = key[:key.find(",")]
+                        wordB = key[key.find(",")+1:]
+                        #print(file, key, value)
+                        similarity = 7 - int(value)
+                        full_norm.append([idd,wordA, wordB, similarity])
+                        full_norm_id.append([idd,word_dict[wordA], word_dict[wordB], similarity])
+                        if key not in avg_dict:
+                            avg_dict[key] = []
+                        avg_dict[key].append(similarity)
+                idd +=1
+
+
+#print(avg_dict)
+for key, value in avg_dict.items():
+    wordA = key[:key.find(",")]
+    wordB = key[key.find(",")+1:]
+    avg_norm.append([wordA, wordB, np.average(value)])
+    avg_norm_id.append([word_dict[wordA], word_dict[wordB], np.average(value)])
+
+write_data(full_norm,target_dir+"verb_sim_basic_1-12.csv")
+write_data(full_norm_id,target_dir+"verb_sim_basic_id_1-12.csv")
+write_data(avg_norm,target_dir+"verb_sim_avg_1-12.csv")
+write_data(avg_norm_id,target_dir+"verb_sim_avg_id_1-12.csv")
