b79023e461983757339f881b150fec4d8665af77
Ellis Cain <50685260+ellissc@users.noreply.github.com>
2021-03-07 09:40:36 -0500

    Image similarity experiment for Esther

---
+++ exps/hsp_word_pairs_esther/consent_wp.js
@@ -0,0 +1,65 @@
+var consent = {
+    type: "survey-multi-choice",
+    questions: [
+        {prompt:"MTurk2",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"Study #0808000094",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"Indiana University",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"INFORMED CONSENT STATEMENT",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"Multimodal Word Learning - Cross-Situational Learning in Adults",options:[]},
+        {prompt: "You are invited to participate in a research study of language learning. We are interested in how people learn words and categories. We are mainly concerned with how humans learn to associate words with referents. We are not interested in the particular behaviors of particular individuals.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"INFORMATION",options:[]},
+        {prompt:"The entire study will last 20 minutes. You will be asked to study some videos/pictures while listening to some sounds/words. You may be asked to learn which sound/word goes with which referent in the visual stimuli. During or after this experiment, your performance will be automatically collected. These data help us assess learning over the course of the experiment.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"RISKS",options:[]},
+        {prompt:"There is potential risk of loss of confidentiality. This risk is minimized by the investigator, as outlined in the confidentiality section.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"BENEFITS",options:[]},
+        {prompt:"There are no direct benefits to the subjects. However, the findings will give us more information about how people link words to object categories.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"NUMBER OF PEOPLE TAKING PART IN THE STUDY",options:[]},
+        {prompt:"If you agree to participate, you will be one of approximately 1,500 people who will be participating in this research study.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"CONFIDENTIALITY",options:[]},
+        {prompt:"Your identity will be held in confidence in reports in which the study may be published. The digital record and data will be saved on a password-protected computer in a locked laboratory. The data collected from you will be made available only to trained experimenters conducting the study and specific organizations that may inspect or copy research data for quality assurance, such as the IU Institutional Review Board or its designees or the Office for Human Research Protections (OHRP). Digital records will be kept indefinitely as endorsed by the National Institutes of Health. Data, with no links to your identity, will be kept indefinitely. If for any reason you decide to withdraw from the study, either while in progress or after completing the procedure, digital records of you will be destroyed.",options:[]},
+        {prompt:"For the protection of your privacy, this research is covered by a Certificate of Confidentiality from the National Institutes of Health. The researchers may not disclose or use any information, documents, or specimens that could identify you in any civil, criminal, administrative, legislative, or other legal proceeding, unless you consent to it. Information, documents, or specimens protected by this Certificate may be disclosed to someone who is not connected with the research:",options:[]},
+        {prompt:"(1) If there is a federal, state, or local law that requires disclosure (such as to report child abuse or communicable diseases);",options:[]},
+        {prompt:"(2) if it is used for other scientific research in a way that is allowed by the federal regulations that protect research subjects",options:[]},
+        {prompt:"(3) for the purpose of auditing or program evaluation by the government or funding agency",options:[]},
+        {prompt:"You should understand that a Certificate of Confidentiality does not prevent you from voluntarily releasing information about yourself. If you want your research information released to an insurer, medical care provider, or any other person not connected with the research, you must provide consent to allow the researchers to release it.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"FUTURE USE",options:[]},
+        {prompt:"Information collected from you for this study may be used for future research studies or shared with other researchers for future research. If this happens, information which could identify you will be removed before any information are shared. Since identifying information will be removed, we will not ask for your additional consent.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"COMPENSATION",options:[]},
+        {prompt:"For participating in this study, you will receive $2.50.",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"CONTACT",options:[]},
+        {prompt:"If you have any questions about the study or the procedures, you may contact the researcher, Chen Yu, at Indiana University, Department of Psychology, 1101 East 10th Street, Bloomington, IN 47405, 812-856-1920, or email chenyu@indiana.edu. For questions about your rights as a research participant or to discuss problems, complaints or concerns about a research study, or to obtain information, or offer input, contact the IU Human Subjects Office at (812) 856-4242 or by email at irb@iu.edu",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"PARTICIPATION",options:[]},
+        {prompt:"Your participation in this study is voluntary.You may choose not to take part or may leave the study at any time.Leaving the study will not result in any penalty or loss of benefits to which you are entitled.Your decision whether or not to participate in this study will not affect your current or future relations with the investigator(s).",options:[]},
+        {prompt:"",options:[]},
+
+        {prompt:"CONSENT",options:[]},
+        {prompt:"By checking below, you acknowledge that you have read and understand the above information, that you are 18 years of age or older, and give your consent to participate in our internet-based study.",options:[]},
+        {prompt:"Thank you for agreeing to participate in our research. Before you begin, please note that the data you provide may be collected and used by Amazon as per its privacy agreement. Additionally, this research is for residents of the United States over the age of 18*; if you are not a resident of the United States and/or under the age of 18, please do not complete this survey", name:"consent", required:"true", options:["I agree to take part in the study."]},
+    ]
+}
